ca3d96cc4347e30701ce9a82bb93c1b55b705004
knaccc <knaccc@users.noreply.github.com>
2020-09-10 01:17:42 +0100

    README updated

--- src/knaccc/monero/address/WalletAddress.java
+++ src/knaccc/monero/address/WalletAddress.java
@@ -116,7 +116,8 @@ public class WalletAddress {
 
     Scalar privateViewKey = new Scalar("7b37d8922245a07244fd31855d1e705a590a9bd2881825f0542ad99cdaba090a");
 
-    System.out.println("subaddress for account index 0, subaddress index 1: " + getSubaddressBase58(privateViewKey, hexToBytes(walletAddress.getPublicSpendKeyHex()), 0, 1));
+    System.out.println("subaddress for account index 0, subaddress index 1: "
+      + WalletAddress.getSubaddressBase58(privateViewKey, hexToBytes(walletAddress.getPublicSpendKeyHex()), 0, 1));
 
 
     // tests
